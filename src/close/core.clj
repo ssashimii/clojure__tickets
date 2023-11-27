9a6a4a03d3d9975cd63e9e82e59169f4dd0a084d
soojinjeong <suejin9488@gmail.com>
2023-11-27 21:38:13 +0100

    Create graph to develop bfs algorithm and read csv file

--- src/close/core.clj
+++ src/close/core.clj
@@ -1,4 +1,88 @@
-(ns close.core)
+(ns close.core
+    (:require [clojure.data.csv :as csv];;Importing libraries for handling CSV data and file I/O
+      [clojure.java.io :as io]))
+
+(defrecord graph[vertices edges])
+;;Define record named 'graph' with fields 'vertices' and 'edges'
+(defn make-graph[];;Define function 'make-graph' to create and return new graph
+      (->graph(ref {})(ref {})))
+
+(defrecord vertex[mark visited neighbors cost-currently flight]);;Define record names
+
+(defn make-vertex[mark];;Define function that takes a 'mark' and creates new vertex
+      (vertex. mark (ref 0)(ref '())(ref 0)(ref '())))
+(defn graph-add-vertex![graph mark];;Define function that adds vertex with specified 'mark'
+      (dosync;;'!'indicates modifying state
+        (alter(:vertices graph) assoc mark(make-vertex mark))))
+
+(defrecord edge [from to mark weight]);;record with fields 'from', 'to', 'mark', and 'weight'
+
+(defn make-edge [from to mark weight]
+      (->edge from to mark weight));;creates and returns a new edge
+(defn graph-edge-key [from to];;returns a sorted list of the two values
+      (->> [from to] sort list));;creating a key for edge lookup in the graph
+(defn graph-add-edge! [graph from to mark weight]
+      (let [vertices (:vertices graph)
+            from-vertex (get @vertices from)
+            to-vertex (get @vertices to)
+            from-vertex-neighbors @(:neighbors from-vertex)
+            to-vertex-neighbors @(:neighbors to-vertex)
+            new-edge (make-edge from to mark weight)
+            new-edge-key (graph-edge-key from to)]
+           (dosync;;update graph's edges with new edge
+             (ref-set (:edges graph) (assoc @(:edges graph) new-edge-key new-edge));;update neighbors of 'from' vertex with 'to'
+             (ref-set (:neighbors from-vertex) (conj from-vertex-neighbors to))
+             (ref-set (:neighbors to-vertex) (conj to-vertex-neighbors from)))))
+
+(defn fetch-csv [csv-file];;reads and parses a CSV file, returning its content
+      (with-open [file (io/reader csv-file)]
+                 (-> file
+                     slurp
+                     csv/read-csv)))
+
+(def cities-file (fetch-csv "src/clojure_airlines/flight_countries.csv"))
+;;load CSV data from csv into 'cities-file'
+(def g (make-graph));;Create empty graph 'g'
+
+(defn csv-graph [cities-file g];;populates graph with vertices and edges based on CSV data
+      (let [existing-vertex-marks (atom [])]
+           (doseq [vector cities-file]
+                  (doseq [vec (vec (take 2 vector))]
+                         (if (not (.contains @existing-vertex-marks vec))
+                           (do ;;add new vertex to the graph with the current vector data
+                             (graph-add-vertex! g (str vec))
+                             (reset! existing-vertex-marks (conj @existing-vertex-marks vec)))))
+                  (graph-add-edge! g;;add new edge to the graph based on the current vector data
+                                   (str (get vector 0))
+                                   (str (get vector 1))
+                                   (str (get vector 0) " " (get vector 1) " " (get vector 2))
+                                   (Integer/parseInt (get vector 2))))))
+
+(csv-graph cities-file g);;populate graph 'g' with data from CSV file
+
+(defn neighbors-graph [graph mark]
+      (if-let [vertex (get @(:vertices graph) mark)];;returns the neighbors of the vertex
+              @(:neighbors vertex) ;;if vertex is found, return neighbors
+              (do (println (str "No corresponding vertex for the mark" mark));;if vertex isn't found print error message and return an empty list
+                  [])))
+
+(defn graph-has-vertex? [graph mark];;checks if vertex with the given 'mark' exists in the graph
+      (contains? @(:vertices graph) mark))
+
+(defn graph-has-edge? [graph from to];;checks if edge from 'from' to 'to' exists in the graph
+      (contains? @(:edges graph) (graph-edge-key from to)))
+
+(defn graph-reset! [graph];;resets 'visited' property of all vertices in the graph to 0
+      (doseq [vertex (vals @(:vertices graph))]
+             (alter (:visited vertex) (constantly 0))))
+
+(defn get-edge-weight [graph from to];;retrieves weight of edge from 'from' to 'to' in the graph
+      (:weight (get @(:edges graph) (graph-edge-key from to))))
+
+(defn reset-costs! [graph];;resets 'cost-currently' property of all vertices in the graph to 0
+      (doseq [vertex (vals @(:vertices graph))]
+             (dosync
+               (ref-set (:cost-currently vertex) 0))))
 
 
 
