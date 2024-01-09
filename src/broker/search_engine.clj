6c09e4169673906f04ad8a5d6d70de2a8b38ab2c
soojinjeong <suejin9488@gmail.com>
2024-01-09 20:37:02 +0100

    Upload required search_engine.clj and sales_routine.clj

---
+++ src/broker/search_engine.clj
@@ -0,0 +1,9 @@
+(ns clojure-airlines.broker.search-engine)
+
+(defn prepare_travel_plan
+  [departure_city destination_city people]
+  ; people is a vector of vectors; each inner vector contains name and year of birth
+  ; Example of input parameters:
+  ; 'Prague' 'Napoli' [['John Smith' 1980] ['Mary Smith' 1983] ['Sindy Smith' 2010]].
+  700
+  )
